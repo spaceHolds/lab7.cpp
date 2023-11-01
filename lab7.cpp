d7eb5c723a363787bb1bd34ce360c35b7cbd3cb3
Ivan <ivancernev19@gmail.com>
2023-11-01 14:38:53 +0200

    first commit

--- lab7.cpp
+++ lab7.cpp
@@ -1,80 +1,81 @@
-﻿﻿#include <iostream>
+﻿﻿#sdinclude <iostream>
 
 using namespace std;
 
-void pz2_6()
-{
-    int cO, steps = 0;
-    cout << "Enter a number:";
-    cin >> cO;
+void dz2_6() {
+    int main() {
+        int c0, steps = 0;
+        std::cout << "Введіть натуральне число: ";
+        std::cin >> c0;
 
-    while (cO != 1) {
-        if (cO % 2 == 0) {
-            cO /= 2;
+        if (c0 <= 0) {
+            std::cout << "Будь ласка, введіть додатнє натуральне число." << std::endl;
+            return 1;
         }
-        else {
-            cO = 3 * cO + 1;
+
+        while (c0 != 1) {
+            if (c0 % 2 == 0) {
+
+                c0 /= 2;
+            }
+            else {
+
+                c0 = 3 * c0 + 1;
+            }
+            steps++;
+            std::cout << "Крок " << steps << ": " << c0 << std::endl;
         }
-        cout << cO << "\n";
-        steps++;
-    }
-
-    cout << "steps = " << steps << "\n";
-}
-
-void pz2_7()
-{
-    double pi4 = 0.;
-    long n;
-
-    cout << "Number of iterations? ";
-    cin >> n;
-
-    for (long i = 0; i < n; i++) {
-        if (i % 2 == 0) {
-            pi4 += 1.0 / (2 * i + 1);
-        }
-        else {
-            pi4 -= 1.0 / (2 * i + 1);
-        }
-    }
-
-    cout.precision(20);
-    cout << "Pi = " << (pi4 * 4.) << "\n";
-}
-
-void pz2_10()
-{
-    int n = 0;
-    cout << "Enter the size of the square: ";
-    cin >> n;
-
-    if (n < 1 || n > 20) {
-        cout << "The number you entered is not in the needed range.";
-    }
-    else {
-        int nSides = n - 2;
-        cout << '+';
-        for (int i = 0; i < nSides; i++)
-            cout << '-';
-        cout << '+' << endl;
-        for (int i = 0; i < nSides; i++) {
-            cout << '|';
-            for (int j = 0; j < nSides; j++)
-                cout << ' ';
-            cout << '|' << endl;
-        }
-        cout << '+';
-        for (int i = 0; i < nSides; i++)
-            cout << '-';
-        cout << '+' << endl;
+        std::cout << "Для досягнення 1 знадобилося " << steps << " кроків." << std::endl;
     }
 }
+
+void dz2_7() {
+        double sum = 0.0;
+        for (int i = 10; i <= 20; i++) {
+            double sqrt_i = sqrt(i);
+            sum += sqrt_i;
+        }
+        std::cout << "The sum from 10 till 20 is:" << sum << std::endl;
+    }
+}
+
+void dz2_10() {
+        double a, b, h;
+        std::cout << "Enter a start meaning a:";
+        std::cin >> a;
+        std::cout << "Enter an end meaning b:";
+        std::cin >> b;
+        std::cout << "Enter a step h:";
+        std::cin >> h;
+
+        if (h <= 0) {
+            std::cout << "h must be positive, pls change a meaning of h.";
+            return 1;
+        }
+        if (a > b) {
+            std::cout << "a must be less then b, pls change the meaning of a.";
+        }
+        std::cout << "--------------------" << std::endl;
+        std::cout << ": X : Y :" << std::endl;
+        std::cout << "--------------------" << std::endl;
+
+        for (double x = a; x <= b; x += h) {
+
+            if (x<a && x>b) {
+                std::cout << "Error, enter another meaning of x.";
+            }
+            else {
+                double y = (8 + (sqrt(x - 7) / (x + 5)));
+                std::cout << ":" << std::setw(5) << x << ":" << std::setw(5) << y << ":" << std::endl;
+            }
+        }
+        std::cout << "--------------------";
+    }
 
 int main()
 {
-    pz2_6();
-    pz2_7();
-    pz2_10();
+    dz2_6();
+    dz2_7();
+    dz2_10();
     return 0;
 }
